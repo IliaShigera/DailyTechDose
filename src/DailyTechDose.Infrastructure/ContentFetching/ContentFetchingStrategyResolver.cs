5027de14a2530f6108819a849f0844a58a35feb3
Ilia <ShigeraIlia@gmail.com>
2024-10-21 11:30:52 -0400

    Update ContentFetchingStrategyResolver.cs

--- src/DailyTechDose.Infrastructure/ContentFetching/ContentFetchingStrategyResolver.cs
+++ src/DailyTechDose.Infrastructure/ContentFetching/ContentFetchingStrategyResolver.cs
@@ -11,12 +11,14 @@ internal sealed class ContentFetchingStrategyResolver : IContentFetchingStrategy
 
     public IContentFetchingStrategy Resolve(Source source)
     {
+        IContentFetchingStrategy? strategy = default;
+        
         if (source.IsRssSupported)
-            return _strategies.OfType<RssContentFetchingStrategy>().FirstOrDefault()!;
+            strategy = _strategies.OfType<RssContentFetchingStrategy>().FirstOrDefault();
         
         // else
         //  return _strategies.OfType<CustomScraperStrategy>().FirstOrDefault();
         
-        throw new NotSupportedException($"Content fetching strategy not supported: {source}");
+        return strategy ?? throw new NotSupportedException($"Content fetching strategy not supported: {source}");
     }
 }
